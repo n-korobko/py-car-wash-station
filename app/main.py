08392eb173ada76c04f981657434084c5fffc59d
Nikita <hkorobko@gmail.com>
2025-10-27 02:20:23 +0200

    Washing Station

--- app/main.py
+++ app/main.py
@@ -15,13 +15,11 @@ class CarWashStation:
         self,
         distance_from_city_center: float,
         clean_power: int,
-        clean_marker: int,
         average_rating: float,
         count_of_ratings: int
     ) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
-        self.clean_marker = clean_marker
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
